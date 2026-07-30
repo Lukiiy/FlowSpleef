8438ab86ba7c824e0fdb2fdffa2d07b23109092d
Lukiiy <lukiiy@proton.me>
2026-07-30 10:21:10 -0300

    Add Item

---
+++ src/main/java/me/lukiiy/spleef/Item.kt
@@ -0,0 +1,28 @@
+package me.lukiiy.spleef
+
+import io.papermc.paper.datacomponent.DataComponentTypes
+import io.papermc.paper.datacomponent.item.TooltipDisplay
+import org.bukkit.Material
+import org.bukkit.NamespacedKey
+import org.bukkit.enchantments.Enchantment
+import org.bukkit.inventory.ItemFlag
+import org.bukkit.inventory.ItemStack
+import org.bukkit.persistence.PersistentDataType
+
+object Item {
+    val KEY = NamespacedKey(Spleef.getInstance(), "item")
+
+    val SHOVEL: ItemStack = create(Material.IRON_SHOVEL) {
+        addItemFlags(ItemFlag.HIDE_ENCHANTS, ItemFlag.HIDE_UNBREAKABLE)
+        setData(DataComponentTypes.UNBREAKABLE)
+        addUnsafeEnchantment(Enchantment.EFFICIENCY, 3)
+        setData(DataComponentTypes.TOOLTIP_DISPLAY, TooltipDisplay.tooltipDisplay().addHiddenComponents(DataComponentTypes.TOOLTIP_DISPLAY, DataComponentTypes.ITEM_NAME).build())
+    }
+
+    val BALL: ItemStack = create(Material.SNOWBALL) {
+        setData(DataComponentTypes.MAX_STACK_SIZE, 6)
+        setData(DataComponentTypes.TOOLTIP_DISPLAY, TooltipDisplay.tooltipDisplay().addHiddenComponents(DataComponentTypes.ITEM_NAME).build())
+    }
+
+    private fun create(material: Material, builder: ItemStack.() -> Unit) = ItemStack.of(material).apply(builder).apply { editPersistentDataContainer { it.set(KEY, PersistentDataType.BOOLEAN, true) } }
+}
