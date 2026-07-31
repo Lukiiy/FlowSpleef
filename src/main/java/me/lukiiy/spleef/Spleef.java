d6501703768f7d2d2217a7352d3f4f9112220f23
Lukiiy <lukiiy@proton.me>
2026-07-31 02:01:54 -0300

    Update main class

--- src/main/java/me/lukiiy/spleef/Spleef.java
+++ src/main/java/me/lukiiy/spleef/Spleef.java
@@ -1,11 +1,15 @@
 package me.lukiiy.spleef;
 
+import me.lukiiy.flow.Flow;
+import me.lukiiy.spleef.map.ArenaAdapter;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Spleef extends JavaPlugin {
+    public final ArenaAdapter worldAdapter = new ArenaAdapter();
+
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        Flow.getInstance().getManager().register(new Entry());
     }
 
     public static Spleef getInstance() {
