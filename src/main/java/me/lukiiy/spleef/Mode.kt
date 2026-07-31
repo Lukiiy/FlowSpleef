68d6d089aa4c47318d0ae1767bebc0ac827bc57e
Lukiiy <lukiiy@proton.me>
2026-07-31 15:18:09 -0300

    Set default ball amount to 3

--- src/main/java/me/lukiiy/spleef/Mode.kt
+++ src/main/java/me/lukiiy/spleef/Mode.kt
@@ -4,8 +4,8 @@ import org.bukkit.inventory.ItemStack
 
 enum class Mode(private val items: Array<ItemStack>) {
     SHOVELS(arrayOf(Item.SHOVEL)),
-    SNOWBALL(arrayOf(Item.BALL)),
-    MIXED(arrayOf(Item.SHOVEL, Item.BALL));
+    SNOWBALL(arrayOf(Item.BALL.asQuantity(3))),
+    MIXED(arrayOf(Item.SHOVEL, Item.BALL.asQuantity(3)));
 
     fun getItems(): Array<ItemStack> = items.clone()
 }
