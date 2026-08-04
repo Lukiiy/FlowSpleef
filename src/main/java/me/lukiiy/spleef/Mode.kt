b23937a3344bdb0df2f90a65f21b4a8286867edd
Lukiiy <lukiiy@proton.me>
2026-08-04 14:34:13 -0300

    Swap BALL to BALL_MINEABLE in Snowball mode

--- src/main/java/me/lukiiy/spleef/Mode.kt
+++ src/main/java/me/lukiiy/spleef/Mode.kt
@@ -4,7 +4,7 @@ import org.bukkit.inventory.ItemStack
 
 enum class Mode(private val items: Array<ItemStack>) {
     SHOVELS(arrayOf(Item.SHOVEL)),
-    SNOWBALL(arrayOf(Item.BALL.asQuantity(3))),
+    SNOWBALL(arrayOf(Item.BALL_MINEABLE.asQuantity(3))),
     MIXED(arrayOf(Item.SHOVEL));
 
     fun getItems(): Array<ItemStack> = items.clone()
